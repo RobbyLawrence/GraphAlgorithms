76869caabefcdc1c06e26cf81b990826f45c0c59
RobbyLawrence <rwl19291@gmail.com>
2025-01-26 16:33:17 -0500

    added dimacs.cpp

---
+++ Homework/Homework1/dimacs.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  // error check
+  if (argc != 2) {
+    cerr << "usage: ./dimacs file" << endl;
+    return 1;
+  }
+  // convert c-style string into c++ string
+  string file;
+  stringstream ss;
+  ss << argv[1];
+  ss >> file;
+  // open file
+  ifstream fin(file);
+  if (!fin.is_open()) {
+    cerr << "Unable to open file" << endl;
+    return 1;
+  }
+  int num_vertices,num_edges;
+  fin >> num_vertices >> num_edges;
+  // create adjacency matrix
+  vector<vector<int> > adjmatrix;
+  adjmatrix.resize(num_vertices,vector<int>(num_vertices,0));
+  int vertex1, vertex2;
+  while (fin >> vertex1 >> vertex2) {
+    adjmatrix[vertex1][vertex2] = 1;
+    adjmatrix[vertex2][vertex1] = 1;
+  }
+  // output
+  for (int i = 0;i<num_vertices;i++) {
+    for (int j = 0;j<num_vertices;j++) {
+      cout << adjmatrix[i][j] << " ";
+    }
+    cout << '\n';
+  }
+  return 0;
+}
