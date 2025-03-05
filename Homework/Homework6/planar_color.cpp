cd45cefc35c2f079e27f0008d105480d2f3b6f64
RobbyLawrence <rwl19291@gmail.com>
2025-03-05 15:44:39 -0500

    moving files

--- Homework/Homework6/planar_color.cpp
+++ Homework/Homework6/planar_color.cpp
@@ -2,153 +2,159 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
-#include <chrono>
-#include <random>
+#include <queue>
 #include <numeric>
 
 using namespace std;
 
-// using an adjacency list instead of matrix for this one
+
 vector<vector<int> > read_graph(const string &file_name, int &num_vertices) {
     ifstream fin(file_name);
     if (!fin.is_open()) {
         cerr << "Unable to open file\n";
         exit(1);
     }
-    int u, v;
-    int max_vertex = -1;
-    vector<pair<int, int> > edges;
-    while (fin >> u >> v) {
-        edges.push_back({u, v});
-        if (u > max_vertex) {
-            max_vertex = u;
-        }
-        if (v > max_vertex) {
-            max_vertex = v;
-        }
-    }
-    num_vertices = max_vertex + 1;
+    int num_edges;
+    fin >> num_vertices >> num_edges;
+
     vector<vector<int> > adj(num_vertices);
-    for (pair<int,int> edge : edges) {
-        u = edge.first;
-        v = edge.second;
+    int u, v;
+    for (int i = 0; i < num_edges; ++i) {
+        fin >> u >> v;
+        if (u >= num_vertices || v >= num_vertices) {
+            cerr << "vertex number is greater than number of vertices\n";
+            exit(1);
+        }
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
     return adj;
 }
-// create indices and order them by degree from high to low
-vector<int> gen_high_low(const vector<int> &degrees) {
+
+vector<int> gen_high_low(vector<int> &degrees) { // taken from my greedy coloring algorithm
     int n = degrees.size();
     vector<int> order(n);
-    // was using loops + .push_back(i) to fill vector, but it took to long so I switched to iota
-    iota(order.begin(),order.end(),0);
-    sort(order.begin(), order.end(), [&degrees](int a, int b) { // comparator function
-        if (degrees[a] != degrees[b]) {
-            return degrees[a] > degrees[b];
-        } else {
-            return a > b;
-        }
+    iota(order.begin(), order.end(), 0);
+    sort(order.begin(), order.end(), [&degrees](int a, int b) {
+        return degrees[a] > degrees[b] || (degrees[a] == degrees[b] && a > b);
     });
     return order;
 }
-// create indices and order them by degree from low to high
-vector<int> gen_low_high(const vector<int> &degrees) {
-    int n = degrees.size();
-    vector<int> order(n);
-    iota(order.begin(),order.end(),0);
-    sort(order.begin(), order.end(), [&degrees](int a, int b) { // comparator function opposite of previous
-        if (degrees[a] != degrees[b]) {
-            return degrees[a] < degrees[b];
+
+vector<int> bfs_order(vector<vector<int> > &adj, int start) {
+    int n = adj.size();
+    vector<int> order;
+    vector<bool> visited(n, false);
+    queue<int> q;
+
+    q.push(start);
+    visited[start] = true;
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        order.push_back(u);
+        for (int v : adj[u]) {
+            if (!visited[v]) {
+                visited[v] = true;
+                q.push(v);
+            }
         }
-        else {
-            return a < b;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i]) {
+            q.push(i);
+            visited[i] = true;
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                order.push_back(u);
+                for (int v : adj[u]) {
+                    if (!visited[v]) {
+                        visited[v] = true;
+                        q.push(v);
+                    }
+                }
+            }
         }
-    });
+    }
 
     return order;
 }
-// create indices and order them by degree randomly
-vector<int> gen_random(int n) {
-    vector<int> order(n);
-    iota(order.begin(),order.end(),0);
-    // had to look up how to use <random> lol
-    random_device rd;
-    mt19937 g(rd());
-    shuffle(order.begin(), order.end(), g);
-    return order;
-}
-// algorithm for greedy coloring
-int greedy_coloring(const vector<vector<int> > &adj, const vector<int> &order) {
+
+vector<int> greedy_coloring(vector<vector<int> > &adj, vector<int> &order) { // also stolen from greedy coloring
     int n = adj.size();
     vector<int> color(n, -1);
     int max_color = -1;
-    vector<bool> used; // used vector to keep track of used colors
+    vector<bool> used;
+
     for (int v : order) {
         if (max_color == -1) {
-            used.assign(1,false);
-        }
-        else {
+            used.assign(1, false);
+        } else {
             used.assign(max_color + 2, false);
         }
 
         for (int neighbor : adj[v]) {
             if (color[neighbor] != -1) {
-                if (color[neighbor] >= (int)used.size() ) {
+                if (color[neighbor] >= (int)used.size()) {
                     used.resize(color[neighbor] + 1, false);
                 }
                 used[color[neighbor]] = true;
             }
         }
+
         int c = 0;
         while (c < (int)used.size() && used[c]) {
             c++;
         }
+
         color[v] = c;
         if (c > max_color) {
             max_color = c;
         }
     }
 
-    return max_color + 1;
+    return color;
+}
+
+void output_colors(vector<int> &colors) {
+    for (size_t i = 0; i < colors.size(); ++i) {
+        cout << i << " " << colors[i] << endl;
+    }
 }
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        cerr << "Usage: " << argv[0] << " <graph_file>" << endl;
+        cerr << "Usage: ./planar_color <file_name>" << endl;
         return 1;
     }
+
     int num_vertices;
     vector<vector<int> > adj = read_graph(argv[1], num_vertices);
-    vector<int> degrees(num_vertices); // easy to construct
+    vector<int> degrees(num_vertices);
     for (int i = 0; i < num_vertices; ++i) {
         degrees[i] = adj[i].size();
     }
 
-    // go through with greedy coloring using each ordering,
-    // and record the time that it takes
-    // I also had to look up how to use <chrono> lol
-    auto start_high = chrono::high_resolution_clock::now();
-    vector<int> order_high = gen_high_low(degrees);
-    int colors_high = greedy_coloring(adj, order_high);
-    auto end_high = chrono::high_resolution_clock::now();
-    chrono::duration<double> elapsed_high = end_high - start_high;
-    // low to high
-    auto start_low = chrono::high_resolution_clock::now();
-    vector<int> order_low = gen_low_high(degrees);
-    int colors_low = greedy_coloring(adj, order_low);
-    auto end_low = chrono::high_resolution_clock::now();
-    chrono::duration<double> elapsed_low = end_low - start_low;
-    // random
-    auto start_random = chrono::high_resolution_clock::now();
-    vector<int> order_random = gen_random(num_vertices);
-    int colors_random = greedy_coloring(adj, order_random);
-    auto end_random = chrono::high_resolution_clock::now();
-    chrono::duration<double> elapsed_random = end_random - start_random;
+    vector<int> order;
+    vector<int> colors;
 
-    cout << "High to low: " << colors_high << " colors, " << fixed << elapsed_high.count() << " sec" << endl;
-    cout << "Low to high: " << colors_low << " colors, " << fixed << elapsed_low.count() << " sec" << endl;
-    cout << "Pseudo-random: " << colors_random << " colors, " << fixed << elapsed_random.count() << " sec" << endl;
-
-    return 0;
+    order = gen_high_low(degrees);
+    colors = greedy_coloring(adj, order);
+    if (*max_element(colors.begin(), colors.end()) + 1 <= 5) {
+        output_colors(colors);
+        return 0;
+    }
+    for (int start = 0; start < num_vertices; ++start) {
+        order = bfs_order(adj, start);
+        colors = greedy_coloring(adj, order);
+        if (*max_element(colors.begin(), colors.end()) + 1 <= 5) {
+            output_colors(colors);
+            return 0;
+        }
+    }
+    cerr << "No 5-coloring found." << endl;
+    return 1;
 }
