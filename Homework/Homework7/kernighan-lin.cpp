969089d842086a5882ab8b313d8523df020e7e96
RobbyLawrence <rwl19291@gmail.com>
2025-03-12 13:14:24 -0400

    renamed files

--- Homework/Homework7/kernighan-lin.cpp
+++ Homework/Homework7/kernighan-lin.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-class Graph { // I wanted to take a class-based approach this week
+class Graph { // I wanted to use a class-based approach this week
 private:
     int num_vertices;
     int num_edges;
@@ -32,8 +32,8 @@ public:
         return adjlist[vertex];
     }
 
-    bool is_edge(int u, int v) const { // useful later on
-        const auto& neighbors = adjlist[u];
+    bool is_edge(int u, int v) const { // this will be useful later
+        const vector<int>& neighbors = adjlist[u];
         return find(neighbors.begin(), neighbors.end(), v) != neighbors.end();
     }
 };
@@ -41,14 +41,14 @@ public:
 class KernighanLin {
 private:
     const Graph& graph;
-    vector<bool> partition;  // true for A, false for B
-    int cut_size; // number of edges crossing
-
-    int d_calc(int vertex) { // calculates d value for a single vertex
-        int internal = 0; // edges to vertices inside partition
+    vector<bool> partition; // true for A, false for B
+    int cut_size;
+    int total_iterations;
+    int total_passes;
+    int calculate_d_value(int vertex) { // calculates d value for single vertex
+        int internal = 0; // num of edges to vertices in same partition
         int external = 0; // vice versa
         bool is_in_a = partition[vertex];
-
         for (int neighbor : graph.get_neighbors(vertex)) {
             if (partition[neighbor] == is_in_a) {
                 internal++;
@@ -56,22 +56,20 @@ private:
                 external++;
             }
         }
-
         return external - internal;
     }
 
-    int calc_gain(int a, int b) {
-        if (partition[a] == partition[b]) { // vertices shouldn't be in the same partition
+    int calc_gain(int a, int b) { // simulates the gain if we swapped a and b
+        if (partition[a] == partition[b]) { // vertices can't be in same partition
             return 0;
         }
-        int gain = d_calc(a) + d_calc(b);
-        if (graph.is_edge(a, b)) {
-            gain -= 2; // lose two from edge
+        int gain = calculate_d_value(a) + calculate_d_value(b);
+        if (graph.is_edge(a, b)) { // lose two from edge contribution
+            gain -= 2;
         }
         return gain;
     }
-
-    int calc_cut() { // calculate initial cut
+    int calc_cut() { // find initial cut size
         int count = 0;
         for (int i = 0; i < graph.get_num_vertices(); i++) {
             for (int neighbor : graph.get_neighbors(i)) {
@@ -82,38 +80,34 @@ private:
         }
         return count / 2; // each edge should get counted twice
     }
-
 public:
-    int total_iterations;
-    int total_passes;
-    KernighanLin(const Graph& g) : graph(g) {
-        int n = graph.get_num_vertices();
-        partition.resize(n); // for balancing partitions
-        for (int i = 0; i < n; i++) {
-            partition[i] = (i < n / 2); // first half in A, second half in B
+    KernighanLin(const Graph& g) : graph(g), total_iterations(0), total_passes(0) {
+        int num_vertices = graph.get_num_vertices();
+
+        partition.resize(num_vertices); // need balanced partition
+        for (int i = 0; i < num_vertices; i++) {
+            partition[i] = (i < num_vertices / 2);  // place first half in A, second half in B
         }
         cut_size = calc_cut();
     }
 
-    // Run the Kernighan-Lin algorithm
     void run_algorithm() {
         int num_vertices = graph.get_num_vertices();
-
         bool improved = true;
-        while (improved) { // also could've done a do while loop
-            // but who actually uses do while loops
+
+        while (improved) { // run the algorithm until we can't find a better solution
             improved = false;
             total_iterations++;
-            vector<bool> available(num_vertices, true); // tracks which vertices are still able to be swapped
+            vector<bool> available(num_vertices, true); // record of available vertices
             vector<int> gain_values;
             vector<int> a_vertices;
             vector<int> b_vertices;
-            // i'm making num_vertices / 10 passes since that worked well during testing
-            for (int pass = 0; pass < num_vertices / 10; pass++) {
+            // chose 10 passes since it ran quickly on graphs ~100 vertices and returned accurate results
+            for (int pass = 0; pass < 10; pass++) {
                 total_passes++;
                 int best_a = -1, best_b = -1;
                 int best_gain = numeric_limits<int>::min();
-                for (int a = 0; a < num_vertices; a++) {
+                for (int a = 0; a < num_vertices; a++) { // find best swap
                     if (!available[a] || !partition[a]) {
                         continue;
                     }
@@ -121,6 +115,7 @@ public:
                         if (!available[b] || partition[b]) {
                             continue;
                         }
+
                         int gain = calc_gain(a, b);
                         if (gain > best_gain) {
                             best_gain = gain;
@@ -132,29 +127,27 @@ public:
                 gain_values.push_back(best_gain);
                 a_vertices.push_back(best_a);
                 b_vertices.push_back(best_b);
-
-                available[best_a] = available[best_b] = false; // mark as used
+                // mark vertices as used
+                available[best_a] = available[best_b] = false;
                 partition[best_a] = false;
                 partition[best_b] = true;
             }
 
-            for (int i = 0; i < num_vertices / 10; i++) { // undo the temp swaps
+            for (int i = 0; i < 10; i++) { // undo the temp swaps
                 partition[a_vertices[i]] = true;
                 partition[b_vertices[i]] = false;
             }
-            int max_k = -1;
+            int max_k = -1; // find max
             int max_gain = 0;
             int current_gain = 0;
-
-            for (int k = 0; k < num_vertices / 10; k++) {
+            for (int k = 0; k < 10; k++) {
                 current_gain += gain_values[k];
                 if (current_gain > max_gain) {
                     max_gain = current_gain;
                     max_k = k;
                 }
             }
-
-            if (max_gain > 0) { // make the swap if the gain was good
+            if (max_gain > 0) { // perform swaps if the gain is positive
                 improved = true;
                 for (int k = 0; k <= max_k; k++) {
                     partition[a_vertices[k]] = false;
@@ -164,8 +157,8 @@ public:
             }
         }
     }
-    // final partitions
-    pair<vector<int>, vector<int> > get_partitions() const {
+
+    pair<vector<int>, vector<int> > get_partitions() const { // grab the final partitions
         vector<int> a, b;
         for (int i = 0; i < graph.get_num_vertices(); i++) {
             if (partition[i]) {
@@ -180,6 +173,10 @@ public:
     int get_cut_size() const {
         return cut_size;
     }
+
+    pair<int, int> get_iteration_stats() const { // just use this to get the private members of Kernighan-Lin
+        return make_pair(total_iterations, total_passes);
+    }
 };
 
 int main(int argc, char* argv[]) {
@@ -187,7 +184,6 @@ int main(int argc, char* argv[]) {
         cerr << "usage: ./kernighan-lin <file_name>" << endl;
         return 1;
     }
-
     ifstream fin(argv[1]);
     if (!fin.is_open()) {
         cerr << "Unable to open file." << endl;
@@ -199,27 +195,25 @@ int main(int argc, char* argv[]) {
 
     Graph graph(num_vertices, num_edges);
 
-    for (int i = 0; i < num_edges; i++) {
+    for (int i = 0; i < num_edges; i++) { // import graph
         int u, v;
         fin >> u >> v;
         graph.add_edge(u, v);
     }
-
     KernighanLin kl(graph);
     kl.run_algorithm();
-
-    auto partitions = kl.get_partitions();
+    pair<vector<int>,vector<int> > partitions = kl.get_partitions();
     int cut_size = kl.get_cut_size();
-
+    pair<int,int> iteration_stats = kl.get_iteration_stats();
     cout << "Cut size: " << cut_size << endl;
-    cout << "Iterations: " << kl.total_iterations << endl;
-    cout << "Total passes: " << kl.total_passes << endl;
+    cout << "Iterations: " << iteration_stats.first << endl;
+    cout << "Total passes: " << iteration_stats.second << endl;
+
     cout << "Partition A: ";
     for (int vertex : partitions.first) {
         cout << vertex << " ";
     }
     cout << endl;
-
     cout << "Partition B: ";
     for (int vertex : partitions.second) {
         cout << vertex << " ";
--- Homework/Homework7/old/kernighan-lin.cpp
+++ Homework/Homework7/old/kernighan-lin.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-class Graph { // I wanted to use a class-based approach this week
+class Graph { // I wanted to take a class-based approach this week
 private:
     int num_vertices;
     int num_edges;
@@ -32,8 +32,8 @@ public:
         return adjlist[vertex];
     }
 
-    bool is_edge(int u, int v) const { // this will be useful later
-        const vector<int>& neighbors = adjlist[u];
+    bool is_edge(int u, int v) const { // useful later on
+        const auto& neighbors = adjlist[u];
         return find(neighbors.begin(), neighbors.end(), v) != neighbors.end();
     }
 };
@@ -41,14 +41,14 @@ public:
 class KernighanLin {
 private:
     const Graph& graph;
-    vector<bool> partition; // true for A, false for B
-    int cut_size;
-    int total_iterations;
-    int total_passes;
-    int calculate_d_value(int vertex) { // calculates d value for single vertex
-        int internal = 0; // num of edges to vertices in same partition
+    vector<bool> partition;  // true for A, false for B
+    int cut_size; // number of edges crossing
+
+    int d_calc(int vertex) { // calculates d value for a single vertex
+        int internal = 0; // edges to vertices inside partition
         int external = 0; // vice versa
         bool is_in_a = partition[vertex];
+
         for (int neighbor : graph.get_neighbors(vertex)) {
             if (partition[neighbor] == is_in_a) {
                 internal++;
@@ -56,20 +56,22 @@ private:
                 external++;
             }
         }
+
         return external - internal;
     }
 
-    int calc_gain(int a, int b) { // simulates the gain if we swapped a and b
-        if (partition[a] == partition[b]) { // vertices can't be in same partition
+    int calc_gain(int a, int b) {
+        if (partition[a] == partition[b]) { // vertices shouldn't be in the same partition
             return 0;
         }
-        int gain = calculate_d_value(a) + calculate_d_value(b);
-        if (graph.is_edge(a, b)) { // lose two from edge contribution
-            gain -= 2;
+        int gain = d_calc(a) + d_calc(b);
+        if (graph.is_edge(a, b)) {
+            gain -= 2; // lose two from edge
         }
         return gain;
     }
-    int calc_cut() { // find initial cut size
+
+    int calc_cut() { // calculate initial cut
         int count = 0;
         for (int i = 0; i < graph.get_num_vertices(); i++) {
             for (int neighbor : graph.get_neighbors(i)) {
@@ -80,34 +82,38 @@ private:
         }
         return count / 2; // each edge should get counted twice
     }
-public:
-    KernighanLin(const Graph& g) : graph(g), total_iterations(0), total_passes(0) {
-        int num_vertices = graph.get_num_vertices();
 
-        partition.resize(num_vertices); // need balanced partition
-        for (int i = 0; i < num_vertices; i++) {
-            partition[i] = (i < num_vertices / 2);  // place first half in A, second half in B
+public:
+    int total_iterations;
+    int total_passes;
+    KernighanLin(const Graph& g) : graph(g) {
+        int n = graph.get_num_vertices();
+        partition.resize(n); // for balancing partitions
+        for (int i = 0; i < n; i++) {
+            partition[i] = (i < n / 2); // first half in A, second half in B
         }
         cut_size = calc_cut();
     }
 
+    // Run the Kernighan-Lin algorithm
     void run_algorithm() {
         int num_vertices = graph.get_num_vertices();
-        bool improved = true;
 
-        while (improved) { // run the algorithm until we can't find a better solution
+        bool improved = true;
+        while (improved) { // also could've done a do while loop
+            // but who actually uses do while loops
             improved = false;
             total_iterations++;
-            vector<bool> available(num_vertices, true); // record of available vertices
+            vector<bool> available(num_vertices, true); // tracks which vertices are still able to be swapped
             vector<int> gain_values;
             vector<int> a_vertices;
             vector<int> b_vertices;
-            // chose 10 passes since it ran quickly on graphs ~100 vertices and returned accurate results
-            for (int pass = 0; pass < 10; pass++) {
+            // i'm making num_vertices / 10 passes since that worked well during testing
+            for (int pass = 0; pass < num_vertices / 10; pass++) {
                 total_passes++;
                 int best_a = -1, best_b = -1;
                 int best_gain = numeric_limits<int>::min();
-                for (int a = 0; a < num_vertices; a++) { // find best swap
+                for (int a = 0; a < num_vertices; a++) {
                     if (!available[a] || !partition[a]) {
                         continue;
                     }
@@ -115,7 +121,6 @@ public:
                         if (!available[b] || partition[b]) {
                             continue;
                         }
-
                         int gain = calc_gain(a, b);
                         if (gain > best_gain) {
                             best_gain = gain;
@@ -127,27 +132,29 @@ public:
                 gain_values.push_back(best_gain);
                 a_vertices.push_back(best_a);
                 b_vertices.push_back(best_b);
-                // mark vertices as used
-                available[best_a] = available[best_b] = false;
+
+                available[best_a] = available[best_b] = false; // mark as used
                 partition[best_a] = false;
                 partition[best_b] = true;
             }
 
-            for (int i = 0; i < 10; i++) { // undo the temp swaps
+            for (int i = 0; i < num_vertices / 10; i++) { // undo the temp swaps
                 partition[a_vertices[i]] = true;
                 partition[b_vertices[i]] = false;
             }
-            int max_k = -1; // find max
+            int max_k = -1;
             int max_gain = 0;
             int current_gain = 0;
-            for (int k = 0; k < 10; k++) {
+
+            for (int k = 0; k < num_vertices / 10; k++) {
                 current_gain += gain_values[k];
                 if (current_gain > max_gain) {
                     max_gain = current_gain;
                     max_k = k;
                 }
             }
-            if (max_gain > 0) { // perform swaps if the gain is positive
+
+            if (max_gain > 0) { // make the swap if the gain was good
                 improved = true;
                 for (int k = 0; k <= max_k; k++) {
                     partition[a_vertices[k]] = false;
@@ -157,8 +164,8 @@ public:
             }
         }
     }
-
-    pair<vector<int>, vector<int> > get_partitions() const { // grab the final partitions
+    // final partitions
+    pair<vector<int>, vector<int> > get_partitions() const {
         vector<int> a, b;
         for (int i = 0; i < graph.get_num_vertices(); i++) {
             if (partition[i]) {
@@ -173,10 +180,6 @@ public:
     int get_cut_size() const {
         return cut_size;
     }
-
-    pair<int, int> get_iteration_stats() const { // just use this to get the private members of Kernighan-Lin
-        return make_pair(total_iterations, total_passes);
-    }
 };
 
 int main(int argc, char* argv[]) {
@@ -184,6 +187,7 @@ int main(int argc, char* argv[]) {
         cerr << "usage: ./kernighan-lin <file_name>" << endl;
         return 1;
     }
+
     ifstream fin(argv[1]);
     if (!fin.is_open()) {
         cerr << "Unable to open file." << endl;
@@ -195,25 +199,27 @@ int main(int argc, char* argv[]) {
 
     Graph graph(num_vertices, num_edges);
 
-    for (int i = 0; i < num_edges; i++) { // import graph
+    for (int i = 0; i < num_edges; i++) {
         int u, v;
         fin >> u >> v;
         graph.add_edge(u, v);
     }
+
     KernighanLin kl(graph);
     kl.run_algorithm();
-    pair<vector<int>,vector<int> > partitions = kl.get_partitions();
-    int cut_size = kl.get_cut_size();
-    pair<int,int> iteration_stats = kl.get_iteration_stats();
-    cout << "Cut size: " << cut_size << endl;
-    cout << "Iterations: " << iteration_stats.first << endl;
-    cout << "Total passes: " << iteration_stats.second << endl;
 
+    auto partitions = kl.get_partitions();
+    int cut_size = kl.get_cut_size();
+
+    cout << "Cut size: " << cut_size << endl;
+    cout << "Iterations: " << kl.total_iterations << endl;
+    cout << "Total passes: " << kl.total_passes << endl;
     cout << "Partition A: ";
     for (int vertex : partitions.first) {
         cout << vertex << " ";
     }
     cout << endl;
+
     cout << "Partition B: ";
     for (int vertex : partitions.second) {
         cout << vertex << " ";
